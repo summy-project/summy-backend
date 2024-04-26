5fc1f0f30579233665c533f65cca53cad078a40f
Luke Na <narukeu@outlook.com>
2024-04-26 17:53:55 +0800

    Swagger 增加说明。

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     const swaggerConfig = new DocumentBuilder()
       .setTitle(`${config.about.name}接口文档`)
       .setDescription(
-        "本项目的 Vue/React 前端在部署和允许的时候已经设置反代，因此实际请求路径以 /api 开头。"
+        "本项目的 Vue/React 前端在部署和允许的时候已经设置反代，因此实际请求路径以 /api 开头。如果诸如 findAll 的查询列表接口是 POST 方法，证明这个接口可以进行模糊查询，具体方法见前端代码。"
       )
       .setVersion(config.about.version)
       .build();
